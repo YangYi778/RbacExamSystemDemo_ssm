5e9aba80512999c152c4a79e8344d4a2ea4d0a45
YangYi778 <531483273@qq.com>
2020-09-08 18:42:46 +0800

    添加用户实体类

---
+++ src/main/java/com/ysu/entity/User.java
@@ -0,0 +1,14 @@
+package com.ysu.entity;
+
+import lombok.Data;
+
+/**
+ * Created by 万恶de亚撒西 on 2020/9/8.
+ */
+@Data
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String status;
+}
